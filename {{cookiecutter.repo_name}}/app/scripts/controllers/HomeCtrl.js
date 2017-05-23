7e6683eec11cf67352bc063bc4517831a4d9bd66
Rob Sterlini <sterlini@fueled.com>
2017-05-23 10:52:30 +0100

    fix(HomeCtrl): Fixed remaining installation blocker

--- {{cookiecutter.repo_name}}/app/scripts/controllers/HomeCtrl.js
+++ {{cookiecutter.repo_name}}/app/scripts/controllers/HomeCtrl.js
@@ -5,11 +5,11 @@ var app = angular.module('{{cookiecutter.base_app_name}}');
 app.controller('HomeCtrl', ['$scope', function ($scope) {
     $scope.message = 'Hello, Home !';
 
-    $scope.exampleForm {
-        model = {},
-        form = {},
-        errors = [],
-        fields = [
+    $scope.exampleForm = {
+        model: {},
+        form: {},
+        errors: [],
+        fields: [
             {
                 label: 'First name',
                 id: 'first_name',
